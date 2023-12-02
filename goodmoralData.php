d57ca11d6e369e6397c4f2e1ef9b16f771e824ef
Angelo <140522134+Solotovvvv@users.noreply.github.com>
2023-12-02 19:17:51 +0800

    modalFetchdata

---
+++ goodmoralData.php
@@ -0,0 +1,30 @@
+<?php
+include 'includes/config.php';
+$pdo = Database::connection();
+
+$id = $_POST['id'];
+$sql = "SELECT *, c.abbreviation FROM `student` s INNER JOIN course c ON s.course = c.id WHERE s.id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+if ($stmt->execute()) {
+    $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($userData) {
+        $data = $userData;
+    } else {
+        $data = array(
+            'status' => 'failed',
+            'message' => 'Data not found'
+        );
+
+    }
+} else {
+    $data = array(
+        'status' => 'failed',
+        'error' => $stmt->errorInfo()
+    );
+
+}
+echo json_encode($data);
+
+?>
