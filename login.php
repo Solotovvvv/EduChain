ceb54f0e13f34da55e0ccd93ac6e5c62d6af9507
Angelo <140522134+Solotovvvv@users.noreply.github.com>
2023-12-02 18:00:44 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+
+<body style="background: #F4F6F9;">
+    <div class="card login-card text-center">
+        <i class="fa-regular fa-user text-muted" style="font-size: 4rem;"></i>
+
+        <p class="mt-4 mb-4">Log in to your account</p>
+
+        <input type="text" class="form-control mb-2" placeholder="Username">
+        <input type="text" class="form-control mb-3" placeholder="Password">
+        <button class="btn btn-primary">LOGIN</button>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+</body>
+
+</html>
